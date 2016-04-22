f3a7e48f4d19b6cc7d1c07b240726ab02e3b4376
koziro2000 <koziro2000@gmail.com>
2016-04-22 12:38:30 -0400

    add date and time pickers
    using jQuery-UI and TimePicker, now I can put date and time for each
    task

---
+++ ng-todo-master/app/appJquery.js
@@ -0,0 +1,3 @@
+$('#new-todo-startDt').datepicker({dateFormat: "yy-M-dd", defaultDate: -1});
+$('#new-todo-endDt').datepicker({dateFormat: "yy-M-dd", defaultDate: -1});
+$('#new-todo-startTm').timepicker({'scrollDefault': 'now', 'setTime': new Date()});
