c2c8012fd94d1cecb63a9861b1811fb00d25e7af
koziro2000 <koziro2000@gmail.com>
2016-04-25 22:02:03 -0400

    modify save to work on both new and modify
    check if the save is called with taskId (not empty). if so then it is
    existing object so update the object. otherwise, create a new object.

--- ng-todo-master/app/todoStorage.js
+++ ng-todo-master/app/todoStorage.js
@@ -22,20 +22,37 @@ angular.module('app').service('todoStorage', function ($q) {
     }
 
     this.save = function (taskId, newContent, newStartDt, newEndDt, status) {
+        //there must be more elegant way to do this but for now this works ....
         var id = this.data.length + 1;
-        var todo = {
-            id: id,
-            content: newContent,
-            startDt: newStartDt,
-            endDt: newEndDt,
-            completed: false,
-            createdAt: new Date(),
-            status: status
-        };
-        this.data.push(todo);
+        var todo = undefined;
+        if(taskId != undefined && taskId != '') {
+            id = taskId;
+            
+            for(var i = 0; i < this.data.length; i++) {
+                if(this.data[i].id == taskId) {
+                    this.data[i].content = newContent;
+                    this.data[i].startDt = newStartDt;
+                    this.data[i].endDt = newEndDt;
+                    this.data[i].status = status;
+                }
+            }
+            
+        } else {
+            todo = {
+                id: id,
+                content: newContent,
+                startDt: newStartDt,
+                endDt: newEndDt,
+                completed: false,
+                createdAt: new Date(),
+                status: status
+            };
+            this.data.push(todo);
+        } 
         this.sync();
     }
 
+    
     this.remove = function(todo) {
         this.data.splice(this.data.indexOf(todo), 1);
         this.sync();
