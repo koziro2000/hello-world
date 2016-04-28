f1a45a025cd265fe1e715cae78bcb0c500fb98a1
koziro2000 <koziro2000@gmail.com>
2016-04-28 09:05:28 -0400

    modify default date and apply bootstrap css
    The default date is now set to 0 instead of -1 for start and 1 instead
    of -1 for end date.
    
    Add css style to table for setting width and look and feel.

--- ng-todo-master/app/appJquery.js
+++ ng-todo-master/app/appJquery.js
@@ -1,4 +1,4 @@
-$('#new-todo-startDt').datepicker({dateFormat: "yy-M-dd", defaultDate: -1});
-$('#new-todo-endDt').datepicker({dateFormat: "yy-M-dd", defaultDate: -1});
+$('#new-todo-startDt').datepicker({dateFormat: "yy-M-dd", defaultDate: 0});
+$('#new-todo-endDt').datepicker({dateFormat: "yy-M-dd", defaultDate: 1});
 $('#new-todo-startTm').timepicker({'scrollDefault': 'now'});
 
